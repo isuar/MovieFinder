91def00af990c99cc746111c67749461e9c64506
isuar <136732411+isuar@users.noreply.github.com>
2025-04-01 22:43:50 +0800

    edit

--- middleware/logger.global.ts
+++ middleware/logger.global.ts
@@ -1,3 +1,3 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  console.log(`Navigating from ${from.path} to ${to.path}`);
-});
+// export default defineNuxtRouteMiddleware((to, from) => {
+//   console.log(`Navigating from ${from.path} to ${to.path}`);
+// });
