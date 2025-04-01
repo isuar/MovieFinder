88ed8af4913386abc9fd8cae10c608629469c2ed
isuar <136732411+isuar@users.noreply.github.com>
2025-04-01 23:57:49 +0800

    edit

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,8 +1,16 @@
-export default defineNuxtRouteMiddleware(() => {
-  const userIsLoggedIn = () => false;
-  if (!userIsLoggedIn()) {
-    return navigateTo("/login");
-  }
+export default defineNuxtRouteMiddleware((to) => {
+  // skip middleware on server
+  if (import.meta.server) return;
+  // skip middleware on client side entirely
+  if (import.meta.client) return;
+  // or only skip middleware on initial client load
+  const nuxtApp = useNuxtApp();
+  if (
+    import.meta.client &&
+    nuxtApp.isHydrating &&
+    nuxtApp.payload.serverRendered
+  )
+    return;
 });
 
 // const userIsLoggedIn = () => false;
