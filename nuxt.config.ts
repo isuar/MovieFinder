ba1e567fa7753435872d36b3a2936e9bf8db0a8c
isuar <136732411+isuar@users.noreply.github.com>
2025-04-08 22:55:54 +0800

    edit rendering mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: "2023-11-04",
   devtools: { enabled: true },
   modules: [
     ["@nuxt/eslint", {}],
