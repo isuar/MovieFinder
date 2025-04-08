baee74a3350d87e7597d2aff860c5a5547cf8573
isuar <136732411+isuar@users.noreply.github.com>
2025-04-08 22:59:34 +0800

    edit rendering mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,64 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2023-11-04",
   devtools: { enabled: true },
   modules: [
-    ["@nuxt/eslint", {}],
     ["@pinia/nuxt", { autoImports: ["defineStore", "acceptHMRUpdate"] }],
   ],
   imports: {
     dirs: ["stores"],
   },
 
-  routeRules: {
-    "/spa": { ssr: false },
-    "/static": { static: true },
-    "/swr": { swr: true },
-  },
-
+  // Force client-side rendering only
   ssr: false,
 
+  // Minimal build configuration
+  app: {
+    buildAssetsDir: "/_nuxt/",
+    baseURL: "./", // Use relative paths
+  },
+
+  experimental: {
+    payloadExtraction: false,
+    renderJsonPayloads: false,
+  },
+
+  // Basic GitHub pages setup without prerendering
   nitro: {
-    hooks: {
-      "prerender:generate"(route) {
-        const routesToSkip = ["/index.html", "/200.html", "/404.html"];
-        if (routesToSkip.includes(route.route)) {
-          route.skip = true;
-        }
-      },
+    preset: "github-pages",
+    prerender: {
+      routes: [],
     },
   },
 });
+
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   compatibilityDate: "2024-11-01",
+//   devtools: { enabled: true },
+//   modules: [
+//     ["@nuxt/eslint", {}],
+//     ["@pinia/nuxt", { autoImports: ["defineStore", "acceptHMRUpdate"] }],
+//   ],
+//   imports: {
+//     dirs: ["stores"],
+//   },
+
+//   routeRules: {
+//     "/spa": { ssr: false },
+//     "/static": { static: true },
+//     "/swr": { swr: true },
+//   },
+
+//   ssr: false,
+
+//   nitro: {
+//     hooks: {
+//       "prerender:generate"(route) {
+//         const routesToSkip = ["/index.html", "/200.html", "/404.html"];
+//         if (routesToSkip.includes(route.route)) {
+//           route.skip = true;
+//         }
+//       },
+//     },
+//   },
+// });
